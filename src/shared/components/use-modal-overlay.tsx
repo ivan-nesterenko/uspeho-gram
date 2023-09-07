613acf4dc8fdff88df4f8ab681419d4b2e634c16
Ivan <91385595+DinGo42@users.noreply.github.com>
2023-09-07 13:48:00 +0300

    added history for widgets

--- src/shared/components/use-modal-overlay.tsx
+++ src/shared/components/use-modal-overlay.tsx
@@ -11,41 +11,48 @@ type ModalOverlayProps = {
 };
 
 type ModalContextProperties = {
-  overlayProps: { isOpen: boolean; content: ReactNode };
+  overlayProps: { isOpen: boolean; content: ReactNode }[];
   openOverlay: (content: ReactNode) => void;
   closeOverlay: () => void;
+  closeAllOverlays: () => void;
 };
 type OverlayProperties = {
   isOpen: boolean;
   content: ReactNode;
 };
 export const overlayContext = createContext<ModalContextProperties>({
-  overlayProps: { isOpen: false, content: <></> },
+  overlayProps: [{ isOpen: false, content: <></> }],
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   openOverlay: () => {},
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   closeOverlay: () => {},
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  closeAllOverlays: () => {},
 });
-
 export const ModalOverlay: FC<ModalOverlayProps> = ({ children }) => {
-  const [overlayProps, setOverlayProps] = useState<OverlayProperties>({
-    isOpen: false,
-    content: <></>,
-  });
+  const [overlayProps, setOverlayProps] = useState<OverlayProperties[]>([]);
   const openOverlay = useCallback((content: ReactNode) => {
-    setOverlayProps({ content: content, isOpen: true });
+    setOverlayProps((prevProps) => [
+      ...prevProps,
+      { content: content, isOpen: true },
+    ]);
   }, []);
   const closeOverlay = useCallback(() => {
-    setOverlayProps({
-      content: <></>,
-      isOpen: false,
+    setOverlayProps((prevProps) => {
+      const updatedProps = [...prevProps];
+      updatedProps.pop();
+      return updatedProps;
     });
   }, []);
+  const closeAllOverlays = useCallback(() => setOverlayProps([]), []);
+  console.log(overlayProps);
   return (
     <overlayContext.Provider
-      value={{ overlayProps, openOverlay, closeOverlay }}
+      value={{ overlayProps, openOverlay, closeOverlay, closeAllOverlays }}
     >
-      {overlayProps.content}
+      {overlayProps.map((modal, index) => (
+        <div key={index}>{modal.content}</div>
+      ))}
       {children}
     </overlayContext.Provider>
   );
