d0bb40c54e9ea7f8d6d4eac62c1dd8d1a0f671f3
Ivan <91385595+DinGo42@users.noreply.github.com>
2023-09-07 13:58:31 +0300

    created account widget

---
+++ src/widgets/menu/settings/account/account.tsx
@@ -0,0 +1,93 @@
+import {
+  BgEffects,
+  Button,
+  ButtonStyleType,
+  Input,
+  InputStyleType,
+  PositionElementAbsolutely,
+  Widget,
+  overlayContext,
+} from "gram/shared";
+import { ArrowIcon, CallIcon, ContactsIcon, EmailIcon } from "public/svgs";
+import { useContext } from "react";
+import { EditWidget, EditWidgetType } from "./edit-widget";
+import { animations, AnimationsTimingKeys } from "gram/utils";
+
+export const Account = () => {
+  const { closeOverlay, openOverlay, closeAllOverlays } =
+    useContext(overlayContext);
+  return (
+    <PositionElementAbsolutely
+      className="justify-center"
+      bgEffects={BgEffects.SHADING}
+      bgOnClick={closeOverlay}
+      contentClassName={animations.slideRight}
+      delay={AnimationsTimingKeys.SHORT}
+    >
+      <Widget className="mt-20">
+        <div className="flex w-full items-center justify-between p-4 text-xl">
+          <div className="flex items-center gap-5">
+            <Button className="p-2" onClick={closeOverlay}>
+              <ArrowIcon />
+            </Button>
+            <span>Info</span>
+          </div>
+          <Button
+            className="h-fit p-2 text-3xl text-black-200 hover:text-white hover:opacity-80"
+            onClick={closeAllOverlays}
+            buttonStyleType={ButtonStyleType.NONE}
+          >
+            ×
+          </Button>
+        </div>
+        <div className="flex flex-col items-center justify-center gap-2 pl-5 pr-5 text-xl">
+          <div className="h-32 w-32 rounded-full bg-orange-400" />
+          <span>Ваня Нестеренко</span>
+          <Input
+            className="text-sm"
+            inputStyleType={InputStyleType.MENU_WIDGET}
+            placeholder="Bio"
+          />
+        </div>
+        <div>
+          <Button
+            buttonStyleType={ButtonStyleType.MENU}
+            className="justify-between"
+            onClick={() => {
+              openOverlay(<EditWidget type={EditWidgetType.name} />);
+            }}
+          >
+            <div className="flex items-center gap-5">
+              <ContactsIcon />
+              Name
+            </div>
+            <span className="text-blue-600">Ваня Нестеренко</span>
+          </Button>
+          <Button
+            buttonStyleType={ButtonStyleType.MENU}
+            className="justify-between"
+          >
+            <div className="flex items-center gap-5">
+              <CallIcon />
+              Phone number
+            </div>
+            <span className="text-blue-600">+380 99 377 42 78</span>
+          </Button>
+          <Button
+            buttonStyleType={ButtonStyleType.MENU}
+            className="justify-between"
+            onClick={() => {
+              openOverlay(<EditWidget type={EditWidgetType.username} />);
+            }}
+          >
+            <div className="flex items-center gap-5">
+              <EmailIcon />
+              Username
+            </div>
+            <span className="text-blue-600">@DinGo42</span>
+          </Button>
+        </div>
+      </Widget>
+    </PositionElementAbsolutely>
+  );
+};
