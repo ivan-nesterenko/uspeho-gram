fdbe690fe8bb2f85211346f84181487601671f3d
Ivan <91385595+DinGo42@users.noreply.github.com>
2023-09-07 13:47:34 +0300

    added animations handler in wrapper

--- src/shared/components/absolute-positioning-utile.tsx
+++ src/shared/components/absolute-positioning-utile.tsx
@@ -1,4 +1,7 @@
-import { type FC, type ReactNode } from "react";
+import { AnimationsTimingKeys, animationsTimings } from "gram/utils";
+import { useState, type FC, type ReactNode } from "react";
+import { twJoin } from "tailwind-merge";
+
 export enum BgEffects {
   SHADING = "opacity-50 bg-black",
   NONE = "",
@@ -9,6 +12,8 @@ type PositionElementAbsolutelyProps = {
   className?: string;
   bgEffects?: BgEffects;
   bgOnClick?: () => void;
+  contentClassName?: ((isOpen: boolean) => string) | string;
+  delay?: AnimationsTimingKeys;
 };
 
 export const PositionElementAbsolutely: FC<PositionElementAbsolutelyProps> = ({
@@ -16,15 +21,31 @@ export const PositionElementAbsolutely: FC<PositionElementAbsolutelyProps> = ({
   className,
   bgEffects = BgEffects.NONE,
   bgOnClick,
+  contentClassName,
+  delay = AnimationsTimingKeys.MEDIUM,
 }) => {
+  const [isOpen, setOpen] = useState(true);
   return (
     <div
       className={`absolute left-0 top-0 flex h-screen w-screen overflow-hidden ${className} z-50`}
     >
-      <div className="z-50">{children}</div>
+      <div
+        className={twJoin(
+          typeof contentClassName === "string"
+            ? contentClassName
+            : contentClassName?.(isOpen),
+          "z-50 transition-all",
+          animationsTimings[delay].className,
+        )}
+      >
+        {children}
+      </div>
       <div
         className={`absolute h-full w-full ${bgEffects}`}
-        onClick={bgOnClick}
+        onClick={() => {
+          setOpen(false);
+          bgOnClick && setTimeout(bgOnClick, animationsTimings[delay].ms);
+        }}
       />
     </div>
   );
