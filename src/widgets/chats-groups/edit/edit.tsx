eebb23e66547bf23923aa69813e7e21e4504eef6
Ivan <91385595+DinGo42@users.noreply.github.com>
2023-09-07 13:53:19 +0300

    created edit widget

--- src/widgets/chats-groups/edit/edit.tsx
+++ src/widgets/chats-groups/edit/edit.tsx
@@ -3,35 +3,39 @@ import {
   Button,
   ButtonStyleType,
   PositionElementAbsolutely,
+  Widget,
   overlayContext,
 } from "gram/shared";
 import { FilesICon } from "public/svgs";
 import { useContext } from "react";
 import { Folder } from "./folder";
+import { AnimationsTimingKeys, animations } from "gram/utils";
 
-export const EditWidget = () => {
-  const { closeOverlay } = useContext(overlayContext);
+export const Edit = () => {
+  const { closeOverlay, closeAllOverlays } = useContext(overlayContext);
 
   return (
     <PositionElementAbsolutely
       bgEffects={BgEffects.SHADING}
       className="justify-center"
       bgOnClick={closeOverlay}
+      contentClassName={animations.fade}
+      delay={AnimationsTimingKeys.SHORT}
     >
-      <div className="mt-20 h-fit w-96 rounded-md bg-black-400 pb-2 text-white">
-        <div className="bg flex w-full items-center justify-between p-1 pl-4 pr-2">
+      <Widget className="mt-20">
+        <div className="bg flex w-full items-center justify-between pb-1 pl-4 pr-2">
           <span>Folders</span>
           <Button
             className="h-fit p-2 text-3xl text-black-200 hover:text-white hover:opacity-80"
-            onClick={closeOverlay}
+            onClick={closeAllOverlays}
             buttonStyleType={ButtonStyleType.NONE}
           >
             ×
           </Button>
         </div>
-        <div className="flex w-full flex-col items-center gap-5 bg-black-350 pb-4 pt-4 text-center text-black-200">
+        <div className="flex w-full flex-col items-center gap-5 bg-black-350 pb-4 pt-4 text-center">
           <FilesICon />
-          <span>
+          <span className="text-black-200">
             Create folders for different groups of chats and quickly switch
             between them.
           </span>
@@ -53,7 +57,7 @@ export const EditWidget = () => {
           </div>
           <span className="text-blue-600">Create new folder</span>
         </Button>
-      </div>
+      </Widget>
     </PositionElementAbsolutely>
   );
 };
