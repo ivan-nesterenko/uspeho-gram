75ddff4fec94b61356423f65af72a26341533798
Ivan <91385595+DinGo42@users.noreply.github.com>
2023-09-11 08:44:27 +0300

    created use overlay hook

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,7 +1,6 @@
 export { AppWrapper } from "./app-wrapper";
 export { Button, ButtonStyleType } from "./button";
 export { MessageCount } from "./message-count";
-export { ModalOverlay, overlayContext } from "./use-modal-overlay";
 export {
   PositionElementAbsolutely,
   BgEffects,
