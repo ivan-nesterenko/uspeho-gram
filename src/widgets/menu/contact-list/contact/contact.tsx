719889d7a4683b86340a5ac2f2ac617a87d5e498
Ivan <91385595+DinGo42@users.noreply.github.com>
2023-09-11 08:50:05 +0300

    fixed styles

--- src/widgets/menu/contact-list/contact/contact.tsx
+++ src/widgets/menu/contact-list/contact/contact.tsx
@@ -1,6 +1,6 @@
 export const Contact = () => {
   return (
-    <div className="flex h-fit w-80 items-center gap-4 overflow-hidden whitespace-nowrap break-words p-1">
+    <div className="max-w-80 flex h-fit items-center gap-4 overflow-hidden whitespace-nowrap break-words p-1">
       <div className="rounded-full bg-orange-400 p-7" />
       <span>Соня</span>
     </div>
