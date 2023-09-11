4458062e897e0a1189859ec4ed8c093e2ce8d8fe
Ivan <91385595+DinGo42@users.noreply.github.com>
2023-09-11 08:45:02 +0300

    updated style

--- src/shared/components/input.tsx
+++ src/shared/components/input.tsx
@@ -11,7 +11,7 @@ export enum InputStyleType {
   CORRESPONDENCE = "bg-black-400 h-full w-full pl-2 text-white border-none",
   CHAT_WINDOW = "bg-black-350 w-full rounded-3xl p-2 pl-6 text-white border-none",
   MENU_WIDGET = "border-b-2 border-black-350 w-full bg-black-400 text-white h-fit pb-1",
-  FORM = "w-full bg-black-600 rounded-md p-3 border-2 border-black-350",
+  FORM = "w-full bg-black-600 rounded-md p-3 border-2 border-black-350 hover:border-purple-800",
   NONE = "",
 }
 export enum ErrorStyleType {
@@ -44,21 +44,29 @@ export const Input = memo(
       ref,
     ) => (
       <div className="relative w-full">
-        <div
-          className={twJoin(
-            inputMessageClassName,
-            "absolute -top-2 left-3 bg-black-700 pl-1 pr-1 text-s text-black-200",
-          )}
-        >
-          {inputMessage}
-        </div>
+        {inputMessage && (
+          <div
+            className={twJoin(
+              "opacity-1 relative left-3 top-[2px] h-[2px] w-fit bg-black-700 pl-2 pr-2 transition-opacity",
+            )}
+          >
+            <span
+              className={twJoin(
+                inputMessageClassName,
+                "relative -top-[700%] text-sm text-black-200",
+              )}
+            >
+              {inputMessage}
+            </span>
+          </div>
+        )}
         <input
           ref={ref}
           className={`${twJoin(
             inputStyleType,
             className,
             isError && errorStyleType,
-          )} outline-none placeholder:text-gray-500 hover:border-purple-800`}
+          )} outline-none placeholder:text-gray-500`}
           {...props}
         />
         {children}
