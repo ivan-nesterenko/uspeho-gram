f15a1c27fd45c1786d92d0f373430fcb42754404
Ivan <91385595+DinGo42@users.noreply.github.com>
2023-09-11 08:43:24 +0300

    added new style

--- src/shared/components/button.tsx
+++ src/shared/components/button.tsx
@@ -26,7 +26,7 @@ import { twJoin } from "tailwind-merge";
 export enum ButtonStyleType {
   GROUP = "p-3 flex items-center justify-center hover:bg-black-300 flex-col text-s w-[4.4rem] text-black-200",
   LEAVE = "p-2 rounded-xl hover:backdrop-brightness-150 hover:text-white transition-all duration-300",
-  MENU = "w-full p-3 flex gap-5 items-center text-white hover:bg-black-350 pl-5 pr-5 text-sm",
+  MENU = "w-full p-3 flex gap-3 items-center text-white hover:bg-black-350 pl-5 pr-5 text-sm",
   WIDGET = "text-blue-600 pl-4 pr-4 p-2 hover:bg-black-300 rounded-md",
   REGULAR = "rounded-md p-1 hover:backdrop-brightness-150 hover:text-white text-black-200",
   SUBMIT = " bg-purple-600 rounded-md p-3 hover:bg-purple-800",
