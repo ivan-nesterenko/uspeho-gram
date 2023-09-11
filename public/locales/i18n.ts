aaa406ec64f95986fc3eedd1154a9cac8fabbc66
Ivan <91385595+DinGo42@users.noreply.github.com>
2023-09-11 08:40:02 +0300

    added locales

--- public/locales/i18n.ts
+++ public/locales/i18n.ts
@@ -1,22 +1,20 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import en from "./en/en.json";
+import ua from "./ua/ua.json";
 
-// the translations
-// (tip move them in a JSON file and import them,
-// or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
 const resources = {
   en: {
     translation: en,
   },
   ua: {
-    translation: en,
+    translation: ua,
   },
 };
 
 void i18n.use(initReactI18next).init({
   resources,
-  lng: "en", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+  lng: "ua", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
   // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
   // if you're using a language detector, do not define the lng option
 
